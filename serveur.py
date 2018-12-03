5e5f1ec8ffb4d322ea614f0807493e9780424f86
baptiste heraud <baptiste.heraud@epitech.eu>
2018-12-02 21:28:44 -0500

    Run server

--- serveur.py
+++ serveur.py
@@ -16,7 +16,29 @@ def checkLogin(user, password):
 		WriteLog("Invalid login " + user, 0, 1)
 		return False
 
+def runServer(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ("0.0.0.0", port)
+    print('starting up on {} port {}'.format(*server_address))
+    try:
+        sock.bind(server_address)
+        sock.listen(1)
+    except socket.error as e:
+        WriteErrorLog(str(e))
+    while True:
+        print('waiting for a connection')
 
+        connection, client_address = sock.accept()
+        try:
+            print('connection from', client_address)
+
+            while True:
+                data = connection.recv(1024)
+                print (data)
+                if not data:
+                    break
+        finally:
+            connection.close()
 
 def WriteLog(msg, type=0, display=1, exit=0):
 	flags = ["ERROR", "INFO", "SUCCESS", "WARNING"]
@@ -42,25 +64,4 @@ if __name__ == "__main__":
 	parser.add_argument("-p", "--port", action="store", dest="port", type=int, default=1337)
 	port = vars(parser.parse_args())["port"]
 
-	# Create a TCP/IP socket
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	# Bind the socket to the port
-	server_address = ('localhost', port)
-	print('starting up on {} port {}'.format(*server_address))
-	try:
-		sock.bind(server_address)
-		sock.listen(5)
-	except socket.error as e:
-		WriteLog(str(e))
-	while True:
-	# Wait for a connection
-		print('waiting for a connection')
-		connection, client_address = sock.accept()
-		try:
-			print('connection from', client_address)
-			while True:
-					data =  connection.recv(1024).decode("utf-8").replace("\r\n", "")
-
-		finally:
-			connection.close()
+	runServer(port)
