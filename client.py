e318481b0223dae9edf3b95c07fef201dde11b86
baptiste <baptiste.heraud@epitech.eu>
2018-12-03 15:58:19 -0500

    UPdate login

--- client.py
+++ client.py
@@ -1,9 +1,22 @@
-import argparse
 import socket
-import smtplib
 import getpass
 from email.mime.text import MIMEText
 
+def runSock(flag):
+    import socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8080))
+    while 1:
+        #data = client_socket.recv(512)
+        user = AboutUser()
+        auth = user[0] + ":" + user[1] + ":"+ str(flag)
+        client_socket.send(bytes(auth, encoding= 'utf-8'))
+        #client_socket.send(bytes(user[1], encoding= 'utf-8'))
+        data = client_socket.recv(512)
+        #if flag == 1:
+        #    Login()
+        #else:
+        #    AddUser()
 
 def displayMenu():
     print("Menu de connexion")
@@ -11,14 +24,12 @@ def displayMenu():
     print("2. Creer un compte")
 
 def AboutUser():
+    login = []
     username = input('Utilisateur : ')
     password = getpass.getpass('Password : ')
-
-def Login():
-    AboutUser()
-
-def AddUser():
-    AboutUser()
+    login.append(username)
+    login.append(password)
+    return login
 
 if __name__ == '__main__':
     flag = 42
@@ -28,7 +39,4 @@ if __name__ == '__main__':
             flag = int(input(''))
         except Exception as ex:
             flag = 42
-    if flag == 1:
-        Login()
-    else:
-        AddUser()
+    runSock(flag)
--- serveur.py
+++ serveur.py
@@ -8,37 +8,41 @@ def send_msg(socket, message):
 	message = message.encode()
 	socket.sendall(message)
 
-def checkLogin(user, password):
-	if user == "baptiste" and password == "test":
-		WriteLog("You are now logged in " + user, 1, 1)
-		return True
+def checkInfo(login):
+	login = login.decode("utf-8")
+	login = login.split(":")
+	print("login = %s" %(login[2]))
+	if login[2] == "1":
+		print ("login")
 	else:
-		WriteLog("Invalid login " + user, 0, 1)
-		return False
+		print ("register")
+	return True
 
 def runServer(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ("0.0.0.0", port)
-    print('starting up on {} port {}'.format(*server_address))
-    try:
-        sock.bind(server_address)
-        sock.listen(1)
-    except socket.error as e:
-        WriteErrorLog(str(e))
-    while True:
-        print('waiting for a connection')
-
-        connection, client_address = sock.accept()
-        try:
-            print('connection from', client_address)
-
-            while True:
-                data = connection.recv(1024)
-                print (data)
-                if not data:
-                    break
-        finally:
-            connection.close()
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_address = ("0.0.0.0", port)
+	print('starting up on {} port {}'.format(*server_address))
+	try:
+		sock.bind(server_address)
+		sock.listen(1)
+	except socket.error as e:
+		WriteErrorLog(str(e))
+	while True:
+		print('waiting for a connection')
+		auth = 0
+		connection, client_address = sock.accept()
+		try:
+			print('connection from', client_address)
+			while True and auth == 0:
+				login = connection.recv(1024)
+				print(login)
+				if not login:
+					break
+				auth = checkInfo(login)
+			print("AUTH")
+				#connection.send(bytes("200", encoding= 'utf-8'))
+		finally:
+			connection.close()
 
 def WriteLog(msg, type=0, display=1, exit=0):
 	flags = ["ERROR", "INFO", "SUCCESS", "WARNING"]
@@ -61,7 +65,7 @@ def WriteLog(msg, type=0, display=1, exit=0):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-p", "--port", action="store", dest="port", type=int, default=1337)
+	parser.add_argument("-p", "--port", action="store", dest="port", type=int, default=8080)
 	port = vars(parser.parse_args())["port"]
 
 	runServer(port)
